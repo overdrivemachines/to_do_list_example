82de3b2c324abb501fda88559950037844f582d1
Dipen Chauhan <get.dipen@gmail.com>
2018-08-17 10:43:42 -0700

    AddPositionToTask

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_173859) do
+ActiveRecord::Schema.define(version: 2018_08_17_174326) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
 end
