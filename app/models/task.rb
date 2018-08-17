9568f9f81ca8e1b02e1289a1f25584f377613b45
Dipen Chauhan <get.dipen@gmail.com>
2018-08-17 11:25:01 -0700

    Acts_as_list

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  completed  :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  position   :integer
+#
+
 class Task < ApplicationRecord
+	acts_as_list
 end
